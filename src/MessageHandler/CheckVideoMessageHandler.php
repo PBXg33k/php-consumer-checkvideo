39fb076b607cceaae146f7aa633fc70e35c89b21
Oguzhan Uysal <development@oguzhanuysal.eu>
2019-12-07 21:31:32 +0100

    Refactor CheckVideoMessageHandler

--- src/MessageHandler/CheckVideoMessageHandler.php
+++ src/MessageHandler/CheckVideoMessageHandler.php
@@ -10,6 +10,7 @@ use Pbxg33k\MessagePack\Message\GenerateThumbnailMessage;
 use App\Message\FooMessage;
 use App\Service\MediaProcessorService;
 use Doctrine\ORM\EntityManagerInterface;
+use Pbxg33k\MessagePack\Message\VideoCheckedMessage;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -31,6 +32,13 @@ class CheckVideoMessageHandler implements MessageHandlerInterface
      */
     private $messageBus;
 
+    /**
+     * @var bool
+     *
+     * Will be set to true if handler is fired from CLI (for testing purposes)
+     */
+    private $interactive = false;
+
     public function __construct(
         MediaProcessorService $mediaProcessorService,
         LoggerInterface $logger,
@@ -39,6 +47,11 @@ class CheckVideoMessageHandler implements MessageHandlerInterface
         $this->mediaProcessorService = $mediaProcessorService;
         $this->logger = $logger;
         $this->messageBus = $messageBus;
+    }
+
+    public function setInteractive(bool $interactive)
+    {
+        $this->interactive = $interactive;
     }
 
     public function __invoke(CheckVideoMessage $message)
@@ -59,40 +72,27 @@ class CheckVideoMessageHandler implements MessageHandlerInterface
                 $message->getPath(),
                 true,
                 function ($type, $buffer) use ($message, $videoLength) {
-                    // Force ping to DBAL to prevent time-out
-                    if ((time() - $startTime) >= 30) {
-                        $this->logger->debug('KEEPALIVE');
-                        $this->messageBus->dispatch(new FooMessage());
-                        $startTime = time();
-                    }
-
                     $callback = $message->getCallback();
                     if (is_callable($callback)) {
                         $callback($type, $buffer);
                     } else {
-                        if (false !== strpos($buffer, ' time=')) {
-//                             Calculate/estimate progress
-//                            if (preg_match('~time=(?<hours>[\d]{1,2})\:(?<minutes>[\d]{2})\:(?<seconds>[\d]{2})?(?:\.(?<millisec>[\d]{0,3}))\sbitrate~', $buffer, $matches)) {
-//                                $time = ($matches['hours'] * 3600 + $matches['minutes'] * 60 + $matches['seconds']) * 1000 + ($matches['millisec'] * 10);
-                                // @todo Pass metadata with CheckVideoMessage
-//                                $this->logger->debug('Progress '.number_format(($time / $javFile->getInode()->getLength()) * 100, 2).'%', [
-//                                    'path' => $javFile->getPath(),
-//                                    'length' => $javFile->getInode()->getLength(),
-//                                    'mark' => $time,
-//                                    'perc' => number_format($time / $javFile->getInode()->getLength() * 100, 2).'%',
-//                                ]);
-//                            }
-//                        } else {
+                        if (false !== strpos($buffer, 'time=')) {
+                            if($videoLength && preg_match('/frame=\s*?(?<frame>[0-9]+)/', $buffer, $matches)) {
+                                $this->logger->info('Progress '.number_format(($matches['frame'] / $videoLength) * 100, 2).'%', [
+                                    'path' => $message->getPath(),
+                                    'frame_count' => $videoLength,
+                                    'current_frame' => $matches['frame'],
+                                    'perc' => number_format(($matches['frame'] / $videoLength) * 100, 2).'%',
+                                ]);
+                            }
+                        } else {
                             $this->logger->debug($buffer);
                         }
                     }
                 }
             );
-//        }
 
-        if($consistent) {
-            $this->messageBus->dispatch(new GenerateThumbnailMessage($message->getPath()));
-            $this->messageBus->dispatch(new CalculateFileHashesMessage($message->getPath(), CalculateFileHashesMessage::HASH_XXHASH | CalculateFileHashesMessage::HASH_MD5));
-        }
+        $this->messageBus->dispatch((new VideoCheckedMessage($message->getPath()))->setChecked(true)->setConsistent($consistent));
+        return $consistent;
     }
 }
